11c60270a397a833bd99aa6ae214e7ef7188d4ad
NikkoleAshleigh <nikkole.hardy.7.1.21@gmail.com>
2022-11-12 14:13:11 -0500

    I never pushed my edits

--- build_library.py
+++ build_library.py
@@ -6,30 +6,38 @@ NOTE: ALL methods for the Library class go in the library.py script
 '''
 
 # 1.0 TODO: Import the Library class
+from library import Library
 
 
 # 2.1 TODO: In `library.py`:
 #   Write the 'constructor' method of the Library class to have one attribute `books`, an empty list.
 #   This function does not need any parameters other than self.
 
+# self.books = []
 
 # 2.2 TODO: In this file:
 #   Create an instance of the `Library` class called `my_library`.
 
+my_library = Library()
 
 # 2.3 TODO: In this file:
 #   Print the `books` attribute of `my_library`. It should be an empty list.
 
+print(my_library.books)
 
 # 2.4 TODO: In this file:
 #   Also print out the `type` of `my_library` and the `type` of its `books` attribute
 
+print(type(my_library))
+print(type(my_library.books))
 
 # 3.1 TODO: In `library.py`:
-#   Fill out the `add()` method to add a book to an instance of the Library class.
+#   Fill out the `add_title()` method to add a book to an instance of the Library class.
 #   - The method should take two parameters (other than self): `title` and `author`, both strings
 #   - The method should create a `Book` object (it's already imported into the file) with those two attributes
 #   - Then, the method should append this book to the `books` attribute of the `Library` object
+
+
 
 
 # 3.2 TODO: In this file:
@@ -39,23 +47,31 @@ NOTE: ALL methods for the Library class go in the library.py script
 #       Sapiens - Yuval Noah Harari
 #       Braiding Sweetgrass - Robin Wall Kimmerer
 #       The Warmth of Other Suns - Isabel Wilkerson
-
+my_library.add_title('The Body Keeps the Score', 'Bessel van der Kolk')
+my_library.add_title('Sapiens', 'Yuval Noah Harari')
+my_library.add_title('Braiding Sweetgrass', 'Robin Wall Kimmerer')
+my_library.add_title('The Warmth of Other Suns', 'Isabel Wilkerson')
 
 # 3.3 TODO: Print the `books` attribute of my_library again to make sure your books have been added.
 
+print(my_library.books)
 
 # 3.4 TODO: Huh. That looks a little weird... Loop through the `books` attribute and print each
 #   one out separately instead.
 
+for book in my_library.books:
+    print(book)
 
 # 4.1 TODO: In `library.py`:
 #   Fill out the `count_books` method to get the number of books in an instance of the `Library` class.
 #   - The method should only take in the `self` parameter, no others
 #   - The method should return the length of the `books` attribute as an integer
-
+# return len(self.books)
 
 # 4.2 TODO: In this script:
 #   Once you have finished the method, count the books in my_library and print out the result
+
+print(my_library.count_books())
 
 
 # 5.1 TODO: In `library.py`:
@@ -64,19 +80,26 @@ NOTE: ALL methods for the Library class go in the library.py script
 #   - The method should remove all books matching the input title from the `Library` instance.
 #       NOTE: We know a `list` method that does something like this, but will it remove ALL matching books?
 #   - The method does not need to return anything
-
+for book in my_library.books:
+    print(book)
 
 # 5.2 TODO: In this script:
 #   Once you have finished the method, remove 'Braiding Sweetgrass' from `my_library`.
+
+my_library.remove_title('Braiding Sweetgrass')
 
 
 # 5.3 TODO: In this script:
 #   Repeat the loop you wrote in 3.4 to check if it worked.
 
+for book in my_library.books:
+    print(book)
 
 # We've now used that loop twice, and any time we want to see the titles in our library,
 #   we'll probably want to use it again. That tells us something: the loop code is a pretty good
 #   candidate to turn into a method of the `Library` class. So let's do it!
+
+
 
 # 6.1 TODO: In `library.py`:
 #   Create a new method called `display_books`.
@@ -88,20 +111,22 @@ NOTE: ALL methods for the Library class go in the library.py script
 
 # 6.2 TODO: In this file:
 #   This time, call your library's `display_books` method to see if it works.
-
+books_to_display = Library.display_books(book)
+print(books_to_display)
 
 # 7.1 TODO: In this script:
 #   Instantiate another instance of the `Library` class called `nyt_bestsellers`
-
+nyt_bestsellers = Library()
 
 # 7.2 TODO: In this script:
 #   Add two books of your choosing from the New York Times best sellers lists to nyt_bestsellers using the .add_title() method
 #   You can find NYT books here: https://www.nytimes.com/books/best-sellers/
-
+nyt_bestsellers.add_title("The Sunflower","Forgot")
+nyt_bestsellers.add_title("The color of water","Forgot")
 
 # 7.3 TODO: In this script:
 #   Call the `display_books` method on the `nyt_bestsellers` Library object.
-
+nyt_bestsellers.display_books()
 
 # BONUS .1 TODO: In `library.py`
 #   Update the `display_books` method to sort the books in alphabetical order firs
